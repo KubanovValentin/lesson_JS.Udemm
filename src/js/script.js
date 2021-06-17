a9f944853482736ca0ac04a924e4418f68177e84
KubanovValentin <delo.biss13@gmail.com>
2021-06-17 13:39:41 +0300

    beginning

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+// const obj = {
+//     name: "John",
+//     age : 25,
+//     isMarried: false
+// };
+
+// console.log(obj.age);
+// console.log(obj["age"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+
+// console.log(arr[2]);
+
+
+// alert('hello!');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?","");
+// console.log(typeof(answer));
+
+// const answer = +prompt("Вам есть 18?","");
+// console.log(typeof(answer));
+
+// const answers = [];
+// answers[0] = prompt('как ваше имя?', '');
+// answers[1] = prompt('как ваша  фамилия?', '');
+// answers[2] = prompt('сколько вам лет?', '');
+// document.write(answers);
+
+// const user = "valek";
+// alert(`Привет,${user}`);
+
